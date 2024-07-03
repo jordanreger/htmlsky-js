f5cdb1df58a325dee8b63e1b0450f9594b3859ef
Jordan Reger <mail@jordanreger.com>
2024-07-02 20:50:10 -0400

    add user feed

--- actor.ts
+++ actor.ts
@@ -1,11 +1,23 @@
-import { ProfileView } from "npm:@atproto/api";
+import {
+  AppBskyActorDefs,
+  AppBskyFeedGetAuthorFeed,
+  AppBskyFeedPost,
+  AppBskyGraphGetFollowers,
+} from "npm:@atproto/api";
 
 import { agent } from "./main.ts";
 import { getDescriptionFacets } from "./facets.ts";
 
+const DateTimeFormat = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "short",
+  timeStyle: "long",
+  timeZone: "UTC",
+  hour12: false,
+});
+
 export default class Actor {
   uri: string;
-  actor: ProfileView;
+  actor: Promise<AppBskyActorDefs.ProfileViewDetailed>;
 
   constructor(uri: string) {
     this.uri = uri;
@@ -19,13 +31,13 @@ export default class Actor {
     return actor;
   }
 
-  async Raw(): string {
+  async Raw(): Promise<string> {
     const actor = await this.actor;
 
     return JSON.stringify(actor, null, 2);
   }
 
-  async HTML(): string {
+  async HTML(prevCursor?: string): Promise<string> {
     const actor = await this.actor;
 
     actor.username = actor.displayName ? actor.displayName : actor.handle;
@@ -65,13 +77,170 @@ export default class Actor {
     }
     <tr>
       <td colspan="2">
-	<b>${actor.followersCount}</b> followers
-	<b>${actor.followsCount}</b> following
+	<a href="./followers/"><b>${actor.followersCount}</b> followers</a>
+	<a href="./follows/"><b>${actor.followsCount}</b> following</a>
 	<b>${actor.postsCount}</b> posts
       </td>
     </tr>
     </table>
     <hr>
+    ${prevCursor ? await this.Feed(prevCursor) : await this.Feed()}
+    `;
+  }
+
+  async Feed(prevCursor?: string): Promise<string> {
+    const actor = await this.actor;
+    let options: AppBskyFeedGetAuthorFeed.QueryParams = { actor: actor.did };
+    if (prevCursor) {
+      options = { actor: actor.did, cursor: prevCursor };
+    }
+    const { data } = await agent.api.app.bsky.feed.getAuthorFeed(options);
+    const { feed, cursor } = data;
+
+    const feedList: string[] = [];
+    feed.forEach((post) => {
+      const postAuthor = post.post.author,
+        reply = post.reply?.root,
+        record: AppBskyFeedPost.Record = post.post.record;
+      let replyAuthor: AppBskyActorDefs.ProfileView | undefined;
+      if (reply) replyAuthor = reply.author;
+      else replyAuthor = undefined;
+      console.log(post);
+      feedList.push(`
+      <tr>
+	<td>
+	${
+        reply
+          ? `
+	  <table>
+	  <tr><td><b>${
+            replyAuthor.displayName
+              ? replyAuthor.displayName
+              : replyAuthor.handle
+          }</b> (@<a href="/profile/${
+            replyAuthor.handle !== "handle.invalid"
+              ? replyAuthor.handle
+              : replyAuthor.did
+          }">${replyAuthor.handle}</a>)</td></tr>
+	  <tr><td>${reply.record.text}</td></tr>
+	  <tr><td>${DateTimeFormat.format(new Date(reply.record.createdAt))}</td></tr>
+	  <tr><td><br></td></tr>
+	</table>
+	<blockquote>
+	`
+          : ""
+      }
+	<table>
+	  ${
+        actor.did !== postAuthor.did
+          ? `<tr><td><i>Reposted by ${
+            actor.displayName ? actor.displayName : actor.handle
+          }</i></td></tr>`
+          : ``
+      }
+	  <tr><td><b>${
+        postAuthor.displayName ? postAuthor.displayName : postAuthor.handle
+      }</b> (@<a href="/profile/${
+        postAuthor.handle !== "handle.invalid"
+          ? postAuthor.handle
+          : postAuthor.did
+      }">${postAuthor.handle}</a>)</td></tr>
+	  <tr><td>${record.text}</td></tr>
+	  <tr><td>${DateTimeFormat.format(new Date(record.createdAt))}</td></tr>
+	  <tr><td><br></td></tr>
+	</table>
+	${post.reply ? "</blockquote>" : ""}
+	</td>
+      </tr>
+      `);
+    });
+
+    if (cursor) {
+      feedList.push(`
+      <tr>
+	<td><br><a href="?cursor=${cursor}">Next page</a></td>
+      </tr>
+      `);
+    }
+
+    return `
+    <table>
+      ${feedList.join("")}
+    </table>
+    `;
+  }
+
+  async Followers(prevCursor?: string): Promise<string> {
+    const actor = await this.actor;
+    let options: AppBskyGraphGetFollowers.QueryParams = { actor: actor.did };
+    if (prevCursor) {
+      options = { actor: actor.did, cursor: prevCursor };
+    }
+
+    const { data } = await agent.api.app.bsky.graph.getFollowers(options);
+    const { followers, cursor } = data;
+
+    const followersList: string[] = [];
+    followers.forEach((follower) => {
+      followersList.push(`
+      <tr>
+	<td><b>${
+        follower.displayName ? follower.displayName : follower.handle
+      }</b> (@${follower.handle})</td>
+      </tr>`);
+    });
+
+    if (cursor) {
+      followersList.push(`
+      <tr>
+	<td><br><a href="?cursor=${cursor}">Next page</a></td>
+      </tr>
+      `);
+    }
+
+    return `
+    <p><a href="..">Back</a></p>
+    <table>
+      ${followersList.join("")}
+    </table>
+    `;
+  }
+
+  async Follows(prevCursor?: string): Promise<string> {
+    const actor = await this.actor;
+    let options: AppBskyGraphGetFollowers.QueryParams = { actor: actor.did };
+    if (prevCursor) {
+      options = { actor: actor.did, cursor: prevCursor };
+    }
+
+    const { data } = await agent.api.app.bsky.graph.getFollows(options);
+    const { follows, cursor } = data;
+
+    const followsList: string[] = [];
+    follows.forEach((follow) => {
+      followsList.push(`
+      <tr>
+	<td><b>${
+        follow.displayName ? follow.displayName : follow.handle
+      }</b> (<a href="/profile/${
+        follow.handle !== "handle.invalid" ? follow.handle : follow.did
+      }">@${follow.handle}</a>)</td>
+      </tr>`);
+    });
+
+    if (cursor) {
+      followsList.push(`
+      <tr>
+	<td><br><a href="?cursor=${cursor}">Next page</a></td>
+      </tr>
+      `);
+    }
+
+    return `
+    <p><a href="..">Back</a></p>
+    <table>
+      ${followsList.join("")}
+    </table>
     `;
   }
 }
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { AtpAgent } from "npm:@atproto/api";
-import { serveDir } from "jsr:@std/http/file-server";
+import { serveDir, serveFile } from "jsr:@std/http/file-server";
 
 import Actor from "./actor.ts";
 
@@ -8,7 +8,15 @@ export const agent = new AtpAgent({ service: "https://public.api.bsky.app" });
 Deno.serve(async (req) => {
   const url = new URL(req.url);
   const path = url.pathname;
+  const redir = `${url.protocol}${url.host}`;
+  const query = new URLSearchParams(url.search);
 
+  // wrangle bots
+  if (path === "/robots.txt") {
+    return serveFile(req, "./static/robots.txt");
+  }
+
+  // serve static files
   if (path.startsWith("/static")) {
     return serveDir(req, { quiet: true });
   }
@@ -16,7 +24,7 @@ Deno.serve(async (req) => {
   // handle trailing slashes
   if (path.at(-1) !== "/") {
     return Response.redirect(
-      `${url.protocol}${url.host}${url.pathname}/`,
+      `${redir}${url.pathname}/`,
     );
   }
 
@@ -25,9 +33,10 @@ Deno.serve(async (req) => {
     const profilePattern = new URLPattern({ pathname: "/profile/:actor/" });
     if (profilePattern.test(url)) {
       const actorName = profilePattern.exec(url)?.pathname.groups.actor;
-      const actor = new Actor(actorName);
-
-      return new Response(await actor.HTML(), html_headers);
+      const actor = new Actor(actorName!);
+      const cursor = query.get("cursor");
+      if (cursor) return new Response(await actor.HTML(cursor), html_headers);
+      else return new Response(await actor.HTML(), html_headers);
     }
 
     // RAW PROFILE
@@ -36,14 +45,37 @@ Deno.serve(async (req) => {
     });
     if (rawProfilePattern.test(url)) {
       const actorName = rawProfilePattern.exec(url)?.pathname.groups.actor;
-      const actor = new Actor(actorName);
+      const actor = new Actor(actorName!);
 
       return new Response(await actor.Raw(), json_headers);
+    }
+
+    // FOLLOWERS, FOLLOWING
+    const pageProfilePattern = new URLPattern({
+      pathname: "/profile/:actor/:page/",
+    });
+    if (pageProfilePattern.test(url)) {
+      const actorName = pageProfilePattern.exec(url)?.pathname.groups.actor;
+      const pageName = pageProfilePattern.exec(url)?.pathname.groups.page;
+      const actor = new Actor(actorName!);
+
+      if (pageName === "followers") {
+        const cursor = query.get("cursor");
+        if (cursor) {
+          return new Response(await actor.Followers(cursor), html_headers);
+        } else return new Response(await actor.Followers(), html_headers);
+      }
+      if (pageName === "follows") {
+        const cursor = query.get("cursor");
+        if (cursor) {
+          return new Response(await actor.Follows(cursor), html_headers);
+        } else return new Response(await actor.Follows(), html_headers);
+      }
     }
   } catch (error) {
     return new Response(
       `<head><meta name="color-scheme" content="light dark"></head>\n${error}`,
-      headers,
+      html_headers,
     );
   }
 
